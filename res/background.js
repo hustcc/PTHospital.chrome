1ae2ec04822ab6a2b862bf15aa808805da4d011a
hustcc <i@atool.org>
2016-05-04 19:38:56 +0800

    fix hospital bug

--- res/background.js
+++ res/background.js
@@ -9,16 +9,14 @@
 	return host;
 }
 
-var PTINFO = null;
 function checkForPTUrl(tabId, changeInfo, tab) {
 	var PT_url = getDomainFromUrl(tab.url).toLowerCase();
-	PTINFO = PTHospitalList[PT_url];
+	var PTINFO = PTHospitalList[PT_url];
 	if(PTINFO) {
 		// 显示图标
 		chrome.pageAction.show(tabId);
 		// 修改提示文字
 	}
-	return PTINFO;
 };
 
 chrome.tabs.onUpdated.addListener(checkForPTUrl);
--- res/popup.js
+++ res/popup.js
@@ -1,11 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var PTINFO = chrome.extension.getBackgroundPage().PTINFO;
-	console.log(PTINFO);
-	if(PTINFO && PTINFO.length == 2){
-		document.getElementById("content-name").innerHTML = PTINFO[0];
-		document.getElementById("content-addr").innerHTML = PTINFO[1];
-	}else{
-		document.getElementById("content-name").innerHTML = "无信息";
-		document.getElementById("content-addr").innerHTML = "无信息";
+    function $_id(id) {
+        return document.getElementById(id);
+    }
+    function getDomainFromUrl(url) {
+		var host = "null";
+		if(typeof url == "undefined" || null == url)
+			url = window.location.href;
+		var regex = /.*\:\/\/([^\/]*).*/;
+		var match = url.match(regex);
+		if(typeof match != "undefined" && null != match)
+			host = match[1];
+		return host;
 	}
+
+	function checkForPTUrl(url) {
+		var PT_url = getDomainFromUrl(url).toLowerCase();
+		return PTHospitalList[PT_url];
+	};
+    chrome.tabs.getSelected(null, function(tab) {
+    	var PTINFO = checkForPTUrl(tab.url);
+    	if(PTINFO && PTINFO.length == 2){
+			document.getElementById("content-name").innerHTML = PTINFO[0];
+			document.getElementById("content-addr").innerHTML = PTINFO[1];
+		}else{
+			document.getElementById("content-name").innerHTML = "...";
+			document.getElementById("content-addr").innerHTML = "...";
+		}
+    });
+	
 });
