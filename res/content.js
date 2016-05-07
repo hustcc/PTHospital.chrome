e82b55f39e3eed5cbe733479b66d409dd1a5ccbc
aTool <i@atool.org>
2016-05-07 10:35:51 +0800

    加入随机因素，防止被屏蔽

--- res/content.js
+++ res/content.js
@@ -2,24 +2,58 @@ function $_id(id) {
     return document.getElementById(id);
 }
 
+var chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+function generateMixed(n) {
+     var res = "";
+     for(var i = 0; i < n ; i ++) {
+         var id = Math.ceil(Math.random()*35);
+         res += chars[id];
+     }
+     return res;
+}
+
+function getRandomNum(Min, Max) {
+    var Range = Max - Min;
+    var Rand = Math.random();
+    return(Min + Math.round(Rand * Range));
+}
+
+function getRandomDOM() {
+    var divs = document.body.childNodes;
+    if (divs.length == 0) {
+        return document.body;
+    }
+    else {
+        return divs[getRandomNum(0, divs.length)];
+    }
+}
+
 function getMaskContainer(name, phone) {
-    var alert_div = $_id("pthospital_chrome_plugin");
+    var divid = generateMixed(getRandomNum(10, 16));
+    var alert_div = $_id(divid);
     if (!alert_div) {
         alert_div = document.createElement("div");
-        alert_div.id = "pthospital_chrome_plugin";
+        alert_div.id = divid;
         alert_div.innerHTML = "《" + name + "》" + chrome.i18n.getMessage("tipText");
-        alert_div.style.display = "block";
-        alert_div.style["background-color"] = "red";
-        alert_div.style.width = "100%";
-        alert_div.style.height = "50px";
-        alert_div.style["font-family"] = "'Source Sans Pro', 'Microsoft Yahei',sans-serif,Arial";
-        alert_div.style["font-size"] = "30px";
-        alert_div.style.color = "black";
-        alert_div.style.position = "fixed"
-        alert_div.style.bottom = "0";
-        alert_div.style["z-index"] = "2147483647";
-        alert_div.style.lineHeight = "50px";
-        document.body.appendChild(alert_div);
+        var cssText = "display:block!important;" +
+                      "background-color:red!important;" +
+                      "width:" + window.screen.availWidth + "px!important;" +
+                      "height:50px!important;" + 
+                      "font-family:'Source Sans Pro', 'Microsoft Yahei',sans-serif,Arial!important;" + 
+                      "font-size:30px!important;" + 
+                      "color:black!important;" + 
+                      "position:fixed!important;" + 
+                      "left:0!important;bottom:0!important;" + 
+                      "z-index:2147483647!important;" + 
+                      "line-height:50px!important;"
+        alert_div.style.cssText = cssText;
+        var divdom = getRandomDOM();
+        if (document.body == divdom) {
+            document.body.appendChild(alert_div);
+        }
+        else {
+            document.body.insertBefore(alert_div, divdom);   
+        }
     }
     return alert_div;
 }
