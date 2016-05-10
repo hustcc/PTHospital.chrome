fd6cc64bd0ba05f1d78516b9d32dc39423488491
aTool <i@atool.org>
2016-05-10 23:06:02 +0800

    update timer

--- res/baidu_search_content.js
+++ res/baidu_search_content.js
@@ -1,25 +1,34 @@
 function showDanger(dom) {
-  var html = '<a style="background-color:red; padding:3px 6px; margin-left:5px;" href="http://trust.baidu.com/vstar/official/intro?type=gw" target="_blank" class="OP_LOG_LINK c-text c-text-public c-text-mult c-gap-icon-left">莆田系</a>';
+  var html = '<a style="background-color:red; padding:3px 6px; margin-left:5px;" href="https://github.com/hustcc/PTHospital.chrome" target="_blank" class="pt-chrome-plugin OP_LOG_LINK c-text c-text-public c-text-mult c-gap-icon-left">莆田系</a>';
   var title = dom.querySelector(".t");
   if (title) {
     title.innerHTML = title.innerHTML + html;
   }
 }
 
-var search_results = document.getElementsByClassName("c-container") || [];
-for (var i = 0; i < search_results.length; i++) {
-  var result = search_results[i];
-  var url = result.querySelector(".c-showurl");
-  if (url) {
-    url = url.innerText;
-    url = url.substring(0, url.indexOf("/"));
-    chrome.runtime.sendMessage({
-      "id": i,
-      "what": "ptinfo_bs", 
-      "url": url}, function(response) {
-        if (response.data && response.data.length >= 2) {
-            showDanger(search_results[response.id]);    
-        }
-    });
+function doBaidu() {
+  var search_results = document.getElementsByClassName("c-container") || [];
+  for (var i = 0; i < search_results.length; i++) {
+    var result = search_results[i];
+    if (result.querySelector(".pt-chrome-plugin")) {
+      continue;
+    }
+    var url = result.querySelector(".c-showurl");
+    if (url) {
+      url = url.innerText;
+      url = url.substring(0, url.indexOf("/"));
+      chrome.runtime.sendMessage({
+        "id": i,
+        "what": "ptinfo_bs", 
+        "url": url}, function(response) {
+          if (response.data && response.data.length >= 2) {
+              showDanger(search_results[response.id]);    
+          }
+      });
+    }
   }
+  clearTimeout(timer);
+  timer = setTimeout(doBaidu, 30);
 }
+var timer = null;
+doBaidu();
